60a2d285310405e0c15ebfcbff662298ba91abf4
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-08-24 18:40:16 +0530

    live link added for ml

--- api/controller/chat_controller.js
+++ api/controller/chat_controller.js
@@ -1,8 +1,38 @@
 import Chat_db from "../models/chat_model.js"
+import axios from "axios"
 
 class chatCotroller
 {
     static saveChat=async(req,res)=>{
+        try {
+            const {question,email,chatId}=req.body
+            console.log("data:",question,email,chatId)
+            const answer = await axios.post('https://sih-fwt9.onrender.com/chat', {
+                message: question
+              });
+            
+
+            console.log(answer.data)  
+
+            const chat = new Chat_db({
+                email,
+                chatId,
+                question,
+                answer:answer.data.response,
+              });
+          
+              // Save chat to MongoDB
+              await chat.save();
+
+              return res.json(answer.data)
+
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static getChat=async(req,res)=>{
         try {
             
         } catch (error) {
